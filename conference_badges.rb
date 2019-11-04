6d398672c06bcba97674ad7ab4dacb486fd99d8f
Wills Blake <richwblake@gmail.com>
2019-11-04 18:52:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,4 +10,5 @@ end
 def assign_rooms( names )
   new_array = Array.new
   names.each_with_index{ |name, index| new_array << "Hello, #{name}, You'll be assigned to room #{index + 1}!" }
+  new_array
 end
