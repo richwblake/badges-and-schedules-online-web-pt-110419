17a2caf78547b4704e306f278ccad75a5899e83d
Wills Blake <richwblake@gmail.com>
2019-11-04 18:44:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker( name )
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator( name )
-  
+def batch_badge_creator( names )
+  names.collect{ |name| badge_maker( name )  }
 end
