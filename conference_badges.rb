9d528b947a9481b6095b79347d0becc7cf51bb6f
Wills Blake <richwblake@gmail.com>
2019-11-04 18:55:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def assign_rooms( names )
   new_array
 end
 
-def printer
-  puts batch_badge_creator
-  puts assign_rooms
+def printer( attendees )
+  puts batch_badge_creator( attendees )
+  puts assign_rooms( attendees )
 end
